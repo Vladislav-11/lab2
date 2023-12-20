4ece74052b24bddb27d2b63f942a0eca700ad8fc
Vladislav-11 <151114115+Vladislav-11@users.noreply.github.com>
2023-12-20 08:55:22 +0500

    Add files via upload

---
+++ lab2.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // ignore: unnecessary_new
+        home: new Scaffold(
+          // ignore: unnecessary_new
+          body: new ListView(children: const [
+            Text('Гиндулина Р.',style: TextStyle(color: Colors.blue),),
+            Divider(),
+            Text('Кучевасов К.',style: TextStyle(color: Color.fromARGB(255, 243, 33, 33)),),
+            Divider(),
+            Text('Рощин Д.',style: TextStyle(color: Color.fromARGB(255, 224, 52, 187)),),
+            Divider(),
+            Text('Антюхов Н.'),
+            Divider(),
+            Text('Бочкарев А.'),
+            Divider(),
+            Text('Ветчинова В.'),
+            Divider(),
+            Text('Галяутдинов В.'),
+            Divider(),
+            Text('Гильметдинов М.'),
+            Divider(),
+            Text('Гладкий М.'),
+            Divider(),
+            Text('Горбачевский А.'),
+            Divider(),
+            Text('Зуев К.'),
+            Divider(),
+            Text('Исаев Д.'),
+            Divider(),
+            Text('Киселев Г.'),
+            Divider(),
+            Text('Максимов М.'),
+            Divider(),
+            Text('Микерин В.'),
+            Divider(),
+            Text('Наговицын И.'),
+            Divider(),
+            Text('Нуртдинов М.'),
+            Divider(),
+            Text('Пилипенко Д.'),
+            Divider(),
+            Text('Сайфитдинов И.'),
+            Divider(),
+            Text('Ситников М.'),
+            Divider(),
+            Text('Теплякова А.'),
+            Divider(),
+            Text('Усманов Д.'),
+            Divider(),
+            Text('Хайрулин Д.'),
+            Divider(),
+            Text('Шенкнехт С.'),
+            Divider(),
+            ])// ListView
+        )// Scaffold
+    )// MaterialApp
+);
